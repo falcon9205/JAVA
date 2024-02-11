cb2937a534cf5855372add9c0cb13c20f0c6b6a7
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-02-11 12:27:47 +0530

    edit files

--- QUESTIONS/temp.java
+++ QUESTIONS/temp.java
@@ -1,42 +1,14 @@
 import java.util.*;
 
-import javax.print.DocFlavor.STRING;
-
 public class temp {
-  public static void solve(int n, String out, int open, int close, List<String> ans) {
-    if (open == 0 && close == 0) {
-      ans.add(out);
-      return;
-    }
-    String op1 = out;
-    String op2 = out;
-    if (open != 0) {
-      op1 += '(' + "";
-      solve(n, op1, open - 1, close, ans);
-    }
-    if (open < close) {
-      op2 += ")" + "";
-      solve(n, op2, open, close - 1, ans);
-    }
-    // } else if (open == close) {
-    // op1 += '(' + "";
-    // op2 += '(' + "";
-    // solve(n, op1, open - 1, close);
-    // } else {
-    // op1 += ')' + "";
-    // op2 += ')' + "";
-    // solve(n, op1, open, close - 1);
-    // }
-    return;
-
-  }
-
   public static void main(String[] args) {
-    int n = 1;
-
-    String out = '(' + "";
-    List<String> ans = new ArrayList<String>();
-    solve(n, out, n - 1, n, ans);
-    System.out.println(ans);
+    LinkedList<Integer> l = new LinkedList<>();
+    l.offer(1);
+    l.add(4);
+    l.remove();
+    l.remove();
+    l.poll();
+    l.poll();
+    System.out.println(l);
   }
 }
