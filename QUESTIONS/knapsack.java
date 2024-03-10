5ad763b0b3075f47f4a39e1fe99e8b74760407fa
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-03-10 20:58:39 +0530

    adding knapsack question

---
+++ QUESTIONS/knapsack.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class knapsack {
+    static int dp[][];
+
+    public static void setter(int w, int n) {
+        dp = new int[w + 1][n + 1];
+        for (int i = 0; i < dp.length; i++)
+            Arrays.fill(dp[i], -1);
+    }
+
+    public static int knapsack(int wt[], int profit[], int w, int n) {
+        if (n == 0 || w == 0)
+            return 0;
+
+        if (dp[w][n] != -1)
+            return dp[w][n];
+        if (wt[n - 1] <= w)
+            return dp[w][n] = Math.max(profit[n - 1] + knapsack(wt, profit, w - wt[n - 1], n - 1),
+                    knapsack(wt, profit, w, n - 1));
+        else
+            return dp[w][n] = knapsack(wt, profit, w, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int profit[] = { 1, 2, 3, 2 };
+        int wt[] = { 4, 5, 1, 2 };
+        int w = 10;
+        int n = profit.length;
+        setter(w, n);
+        System.out.println(knapsack(wt, profit, w, n));
+    }
+}
--- QUESTIONS/temp.java
+++ QUESTIONS/temp.java
@@ -1,114 +1,29 @@
 import java.util.*;
 
 public class temp {
-  static class Hashmap<K, V> {
-    public static final int DEFAULT_CAPACITY = 4; // default capacity
-    public static final float LOAD_FACTOR = 0.75f; // load factor
+  public static int getCommon(int[] nums1, int[] nums2) {
 
-    private class Node {
-      K key;
-      V value;
-
-      Node(K key, V value) {
-        this.key = key;
-        this.value = value;
-      }
-    }
-
-    private LinkedList<Node> buckets[];
-    private int n; // record entries in map
-
-    @SuppressWarnings("unchecked")
-    public Hashmap() {
-      buckets = new LinkedList[DEFAULT_CAPACITY];
-      for (int i = 0; i < buckets.length; i++) {
-        buckets[i] = new LinkedList<Node>();
-      }
-    }
-
-    private int hashFunc(K key) {
-      int hc = key.hashCode();
-      return Math.abs(hc) % buckets.length;
-    }
-
-    private int searchInBucket(K key, LinkedList<Node> list) {
-      for (int i = 0; i < list.size(); i++) {
-        if (Objects.equals(list.get(i).key, key)) {
-          return i;
-        }
+    Set<Integer> list = new HashSet<Integer>();
+    if (nums1.length >= nums2.length) {
+      for (int i = 0; i < nums1.length; i++)
+        list.add(nums1[i]);
+      for (int i = 0; i < nums2.length; i++) {
+        if (list.contains(nums2[i]))
+          return nums2[i];
       }
       return -1;
-    }
-
-    public int size() {
-      return n;
-    }
-
-    private void init(int n) {
-      buckets = new LinkedList[n];
-      for (int i = 0; i < buckets.length; i++)
-        buckets[i] = new LinkedList<Node>();
-    }
-
-    public void put(K key, V value) {
-      int bi = hashFunc(key);
-      LinkedList<Node> currBuc = buckets[bi];
-      int ei = searchInBucket(key, currBuc);
-      if (ei == -1) { // insertion case
-        Node temp = new Node(key, value);
-        currBuc.add(temp);
-        n++;
-      } else { // update case
-        Node temp = currBuc.get(ei);
-        temp.value = value;
+    } else {
+      for (int i = 0; i < nums2.length; i++)
+        list.add(nums2[i]);
+      for (int i = 0; i < nums1.length; i++) {
+        if (list.contains(nums1[i]))
+          return nums1[i];
       }
-      if (n >= buckets.length * LOAD_FACTOR) {
-        reshash();
-      }
-    }
-
-    private void reshash() {
-      LinkedList<Node> oldbucket[] = buckets;
-      init(oldbucket.length * 2);
-      n = 0;
-      for (var bucket : oldbucket)
-        for (var node : bucket)
-          put(node.key, node.value);
-    }
-
-    public V get(K key) {
-      int bi = hashFunc(key);
-      LinkedList<Node> currBuc = buckets[bi];
-      int ei = searchInBucket(key, currBuc);
-      if (ei != -1) {
-        Node temp = currBuc.get(ei);
-        return temp.value;
-      }
-      return null;
-    }
-
-    public V remove(K key) {
-      int bi = hashFunc(key);
-      LinkedList<Node> currBuc = buckets[bi];
-      int ei = searchInBucket(key, currBuc);
-      if (ei != -1) {
-        Node currNode = currBuc.remove(ei);
-        n--;
-        return currNode.value;
-      }
-      return null;
+      return -1;
     }
   }
 
   public static void main(String[] args) {
-    Hashmap<Integer, String> local = new Hashmap<>();
-    local.put(1, "he");
-    local.put(2, "hel");
-    local.put(3, "hew");
-    local.put(4, "pop");
-    local.put(5, "pip");
-    System.out.println(local.size());
-    System.out.println(local.remove(2));
-    System.out.println(local.size());
+
   }
 }
